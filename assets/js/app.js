cfc28b047ee3e515e1ec21d627247887dfc8ef8a
Dannar Mawardi <dmawardi@gmail.com>
2019-07-20 13:13:45 +1000

    API call function complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,20 +1,28 @@
 // here
 // Function accepts 4 parameters and calls NYTimes for result articles using parameters to filter
-function NYAPICall(title, startYear, endYear, limit) {
+function NYAPICall(title, startYear, endYear) {
     var apiKey = 'CV4eBGk9tBKkxXZ3NcAeOPzq51NT5ifM';
-    // Convert years to required 8 digit values and prepare variables for string concatenation
-    startYear = '&begin_date='+startYear+'0101';
-    endYear = '&end_date='+endYear+'0101';
-    limit = '&limit='+limit;
+    // Convert parameters, if found, for string concatenation. If not, make empty string
+    if (startYear === undefined) {
+        startYear = '';
+    } else {
+        startYear = '&begin_date=' + startYear + '0101';
+
+    }
+    if (endYear === undefined) {
+        endYear = '';
+    } else {
+        endYear = '&end_date=' + endYear + '0101';
+    }
 
     // Build API Query
-    var query = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key='+apiKey+'&q='+title +startYear+endYear+;
+    var query = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' + apiKey + '&q=' + title + startYear + endYear;
 
     console.log(query);
     $.ajax({
         url: query,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
         console.log(response);
     });
 }
