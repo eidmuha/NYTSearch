c1b1303c65b24a7e2b3ebbcbc32417ea56759818
Jinal Shah <jin.shah1988@gmail.com>
2019-07-20 13:55:06 +1000

    changes

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,28 +1,58 @@
 // here
 // Function accepts 4 parameters and calls NYTimes for result articles using parameters to filter
 function NYAPICall(title, startYear, endYear) {
-    var apiKey = 'CV4eBGk9tBKkxXZ3NcAeOPzq51NT5ifM';
-    // Convert parameters, if found, for string concatenation. If not, make empty string
-    if (startYear === undefined) {
-        startYear = '';
-    } else {
-        startYear = '&begin_date=' + startYear + '0101';
+  var apiKey = "CV4eBGk9tBKkxXZ3NcAeOPzq51NT5ifM";
+  // Convert parameters, if found, for string concatenation. If not, make empty string
+  if (startYear === undefined) {
+    startYear = "";
+  } else {
+    startYear = "&begin_date=" + startYear + "0101";
+  }
+  if (endYear === undefined) {
+    endYear = "";
+  } else {
+    endYear = "&end_date=" + endYear + "0101";
+  }
 
+  // Build API Query
+  var query =
+    "https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=" +
+    apiKey +
+    "&q=" +
+    title +
+    startYear +
+    endYear;
+
+  console.log(query);
+  $.ajax({
+    url: query,
+    method: "GET"
+  }).then(function(response) {
+    var results = response.response.docs;
+
+    for (var i = 0; i < 5; i++) {
+      var url = results[i].web_url;
+      var titile = results[i].snippet;
+      var details = results[i].lead_paragraph;
+
+      $("#data-display").append(
+        "<div class='card-header bg-light text-dark'><span> " +
+          (i + 1) +
+          ". </span><span>" +
+          titile +
+          "</span><p></div>"
+      );
+
+      var multimedia = results[i].multimedia;
+      if (multimedia.length > 0) {
+        console.log(multimedia[0].url);
+      }
     }
-    if (endYear === undefined) {
-        endYear = '';
-    } else {
-        endYear = '&end_date=' + endYear + '0101';
-    }
-
-    // Build API Query
-    var query = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' + apiKey + '&q=' + title + startYear + endYear;
-
-    console.log(query);
-    $.ajax({
-        url: query,
-        method: "GET"
-    }).then(function (response) {
-        console.log(response);
-    });
+  });
 }
+
+$("#search").on("click", function(event) {
+  event.preventDefault();
+  console.log("clickd");
+  NYAPICall();
+});
